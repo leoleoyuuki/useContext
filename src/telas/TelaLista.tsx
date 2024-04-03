1b628c9e3da195816c823b705a21ff6692bdce11
leoleoyuuki <rm550373@fiap.com.br>
2024-04-03 11:54:30 -0300

    .

--- src/telas/TelaLista.tsx
+++ src/telas/TelaLista.tsx
@@ -1,13 +1,23 @@
-import { Avatar, Image, ListItem, Text } from "@rneui/base";
+import { Avatar, Button, Icon, Image, ListItem, Text } from "@rneui/base";
 import { FlatList } from "react-native";
 import dadosUsuario from "../dados/dadosUsuario";
 
 function getUsuarios({ item }) {
-  return(
-     <ListItem>
-        <Avatar  />
-     </ListItem>
-  )
+  return (
+    <ListItem>
+      <Avatar source={{ uri: item.fotoPerfil }} rounded size={60} />
+      <ListItem.Content>
+        <ListItem.Title>{item.nome}</ListItem.Title>
+        <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+      </ListItem.Content>
+
+      <ListItem.Content right>
+        <Button 
+            icon={<Icon name="delete" />} 
+            type="clear" />
+      </ListItem.Content>
+    </ListItem>
+  );
 }
 
 export default (props) => {
